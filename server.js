af22aeb9f2fdb41ac16c51ea60bc537ddffa7404
Etienne Warneck <etienne.warneck@gmail.com>
2020-02-15 09:57:47 -0500

    connection to MONGODB

--- server.js
+++ server.js
@@ -20,7 +20,11 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/packt_db", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/packt_db", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/packt_db";
+
+mongoose.connect(MONGODB_URI);
+
 
 // Routes
 app.get("/scrape", function (req, res) {
